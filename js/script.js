f6b0e615d763646e0f2acee7db519cd3ff63f4a9
mario-ganic <62038359+mario-ganic@users.noreply.github.com>
2020-08-17 22:31:31 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const addButton = document.querySelector('#addButton');
 const body = document.querySelector('body');
 const todoList = document.querySelector('#todoList');
-
+const inputText = document.querySelector('input')
 
 addButton.addEventListener('click', () => {
+const inputTextValue = inputText.value;	
 const listItem = document.createElement('li');	
 todoList.appendChild(listItem);
-listItem.innerText = 'List Stuff';
+listItem.innerText = inputTextValue;
 });
