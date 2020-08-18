db0771e2cb8e240f9c2ced5fcd03ae5bd641333a
mario-ganic <62038359+mario-ganic@users.noreply.github.com>
2020-08-18 18:55:53 +0200

    more buttons

--- js/script.js
+++ js/script.js
@@ -6,24 +6,31 @@ const todoList = document.querySelector('#todoList');
 const inputText = document.querySelector('input')
 
 // ADD ITEM TO LIST
-addButton.addEventListener('click', () => {
-    const inputTextValue = inputText.value;
-    const listItem = document.createElement('li');
-    todoList.appendChild(listItem);
-    listItem.innerText = inputTextValue;
-    createButton(listItem);
-    inputText.value = '';
-});
+addButton.addEventListener('click', createListElement);
 
+// CREATE LIST ITEM
 
-// DELETE ITEM
-
-function createButton(e) {
-	 const deleteButton = document.createElement('button');
-   deleteButton.innerText = 'Button';	
-	 e.appendChild(deleteButton);
-	 deleteButton.addEventListener('click', (e) => {
-        e.target.parentElement.remove();
-    })
+function createListElement() {
+	  		const inputTextValue = inputText.value;
+        const listItem = document.createElement('li');
+        todoList.appendChild(listItem);
+        listItem.innerText = inputTextValue;
+        createButton(listItem);
+        inputText.value = '';
 }
 
+// CREATE DELETE AND CHECK BUTTONS
+function createButton(e) {
+    const deleteButton = document.createElement('button');
+    const checkButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    checkButton.innerText = 'Completed';
+    e.appendChild(deleteButton);
+    e.appendChild(checkButton);
+    deleteButton.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
+    })
+    checkButton.addEventListener('click', (e) => {
+        e.target.parentElement.classList.add('marked');
+    })
+}
