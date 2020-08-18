0d4063a9d9beab8875a4f093fb70868b8bf81152
mario-ganic <62038359+mario-ganic@users.noreply.github.com>
2020-08-18 17:56:45 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
+// SELECTORS
+
 const addButton = document.querySelector('#addButton');
 const body = document.querySelector('body');
 const todoList = document.querySelector('#todoList');
 const inputText = document.querySelector('input')
 
+// ADD ITEM TO LIST
 addButton.addEventListener('click', () => {
-const inputTextValue = inputText.value;	
-const listItem = document.createElement('li');	
-todoList.appendChild(listItem);
-listItem.innerText = inputTextValue;
-inputText.value = '';
+    const inputTextValue = inputText.value;
+    const listItem = document.createElement('li');
+    todoList.appendChild(listItem);
+    listItem.innerText = inputTextValue;
+    createButton(listItem);
+    inputText.value = '';
 });
+
+
+// DELETE ITEM
+
+function createButton(e) {
+	 const deleteButton = document.createElement('button');
+   deleteButton.innerText = 'Button';	
+	 e.appendChild(deleteButton);
+	 deleteButton.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
+    })
+}
+
