d4bae1d699fb0239aa281c5293964a15b881d757
mario-ganic <62038359+mario-ganic@users.noreply.github.com>
2020-08-20 23:27:52 +0200

    messing with css

--- js/script.js
+++ js/script.js
@@ -55,3 +55,39 @@ function addListWithEnter(event) {
 addButton.addEventListener('click', createListElement);
 
 inputText.addEventListener('keypress', addListWithEnter);
+
+
+//DATE AND TIME//
+
+
+const today = document.querySelector('#date');
+const date = new Date();
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+} 
+
+
+
+
+today.innerText = day + '  ' +date.getDate() +'-'+(date.getMonth()+1)+'-'+ date.getFullYear();
+
+
