213bfe869a789b85d42fdd077f888d9d9e7bb861
mario-ganic <62038359+mario-ganic@users.noreply.github.com>
2020-12-07 23:29:26 +0100

    minor changes

--- js/script.js
+++ js/script.js
@@ -8,36 +8,36 @@ const inputText = document.querySelector('input');
 // CREATE TO DO
 
 function createListElement() {
-	if(inputText.value.length > 0){
+    if (inputText.value.length > 0) {
         const listItem = document.createElement('li');
         const todoDiv = document.createElement('div');
-				todoList.appendChild(todoDiv);
-				todoDiv.appendChild(listItem);
-				todoDiv.classList.add('todoDiv');
+        todoList.appendChild(todoDiv);
+        todoDiv.appendChild(listItem);
+        todoDiv.classList.add('todoDiv');
         listItem.innerText = inputText.value;
-         checkButton(todoDiv);
+        checkButton(todoDiv);
         deleteButton(todoDiv);
-       
+
         inputText.value = '';
-}
+    }
 }
 
 // CREATE DELETE AND CHECK BUTTONS
 function checkButton(e) {
-	const checkButton = document.createElement('button');
-	checkButton.innerText = 'Finished';
-	checkButton.classList.add('checkBtn');
-  e.appendChild(checkButton);
-   checkButton.addEventListener('click', (e) => {
-   			e.target.parentElement.classList.toggle('marked');
+    const checkButton = document.createElement('button');
+    checkButton.innerText = 'Finished';
+    checkButton.classList.add('checkBtn');
+    e.appendChild(checkButton);
+    checkButton.addEventListener('click', (e) => {
+        e.target.parentElement.classList.toggle('marked');
     })
 }
 
-function deleteButton(element) {
-		const deleteButton = document.createElement('button');
+function deleteButton(e) {
+    const deleteButton = document.createElement('button');
     deleteButton.innerText = 'Delete';
-    element.appendChild(deleteButton);
     deleteButton.classList.add('delBtn');
+    e.appendChild(deleteButton);
     deleteButton.addEventListener('click', (element) => {
         element.target.parentElement.remove();
     })
@@ -66,31 +66,29 @@ inputText.addEventListener('keypress', addListWithEnter);
 const today = document.querySelector('#date');
 const date = new Date();
 switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-     day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-} 
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
 
 
 
 
-today.innerText = day + '  ' +date.getDate() +'-'+(date.getMonth()+1)+'-'+ date.getFullYear();
-
-
+today.innerText = day + '  ' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
